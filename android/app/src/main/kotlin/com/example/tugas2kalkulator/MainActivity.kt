61718098106cb35d44b3773e98dc678bc2b19254
dickygunawan11 <dicky.reborn11122002@gmail.com>
2024-05-05 17:43:38 +0700

    fisrt commit

---
+++ android/app/src/main/kotlin/com/example/tugas2kalkulator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas2kalkulator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomeApp(),
+    );
+  }
+}
+
+class MyHomeApp extends StatefulWidget {
+  _MyHomeAppState createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  TextEditingController textfield1 = TextEditingController();
+  TextEditingController textfield2 = TextEditingController();
+  double hasil = 0, angka1 = 0, angka2 = 0;
+  add() {
+    setState(() {
+      angka1 = double.parse(textfield1.text);
+      angka2 = double.parse(textfield2.text);
+      hasil = angka1 + angka2;
+    });
+  }
+
+  minus() {
+    setState(() {
+      angka1 = double.parse(textfield1.text);
+      angka2 = double.parse(textfield2.text);
+      hasil = angka1 - angka2;
+    });
+  }
+
+  bag() {
+    setState(() {
+      angka1 = double.parse(textfield1.text);
+      angka2 = double.parse(textfield2.text);
+      hasil = angka1 / angka2;
+    });
+  }
+
+  kali() {
+    setState(() {
+      angka1 = double.parse(textfield1.text);
+      angka2 = double.parse(textfield2.text);
+      hasil = angka1 * angka2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(212, 255, 0, 1),
+        title: Text('Kalkulator'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              'Hasil: $hasil ',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: textfield1,
+              decoration: InputDecoration(
+                labelText: 'Masukan Angka pertama',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: textfield2,
+              decoration: InputDecoration(
+                labelText: 'Masukan Angka Kedua',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(212, 255, 0, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                  ),
+                  onPressed: () {
+                    add();
+                    textfield1.clear();
+                    textfield2.clear();
+                  },
+                  child: const Text(
+                    '+',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(212, 255, 0, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                  ),
+                  onPressed: () {
+                    minus();
+                    textfield1.clear();
+                    textfield2.clear();
+                  },
+                  child: const Text(
+                    '-',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 50,
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(212, 255, 0, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                    ),
+                    onPressed: () {
+                      bag();
+                      textfield1.clear();
+                      textfield2.clear();
+                    },
+                    child: const Text(
+                      ':',
+                      style: TextStyle(color: Colors.black),
+                    )),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(212, 255, 0, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 200, vertical: 10),
+                  ),
+                  onPressed: () {
+                    kali();
+                    textfield1.clear();
+                    textfield2.clear();
+                  },
+                  child: const Text(
+                    'x',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
